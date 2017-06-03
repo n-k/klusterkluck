baca293309f4ddbdb2b52b3c6924db8e98313749
nk <nipunkumar@outlook.com>
2017-06-03 23:54:03 +0530

    select where to serve static file from, depending on env; add jvm memory and gc params to image CMD

--- admin/src/main/java/com/github/nk/klusterfuck/admin/WebConfig.java
+++ admin/src/main/java/com/github/nk/klusterfuck/admin/WebConfig.java
@@ -1,5 +1,6 @@
 package com.github.nk.klusterfuck.admin;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -11,18 +12,25 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Order(value = 0)
 public class WebConfig extends WebMvcConfigurerAdapter {
 
+    @Value("${app.env}")
+    private Env env;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         super.addResourceHandlers(registry);
-        registry.addResourceHandler("/**")
-                .addResourceLocations(
-                        "file:./admin/src/main/ui/dist/"
-                )
-                .resourceChain(false);
-//        registry.addResourceHandler("/**")
-//                .addResourceLocations(
-//                        "classpath:static/"
-//                )
-//                .resourceChain(false);
+        if (env == Env.dev) {
+            registry.addResourceHandler("/**")
+                    .addResourceLocations(
+                            "file:./admin/src/main/ui/dist/"
+                    )
+                    .resourceChain(false);
+        } else {
+            registry.addResourceHandler("/**")
+                    .addResourceLocations(
+                            "classpath:static/"
+                    )
+                    .resourceChain(false);
+        }
     }
+
 }
