be4107a048e6b7b2df970dad7a31c4226ddbebbd
nk <nipunkumar@outlook.com>
2017-06-12 15:54:57 +0530

    Moved agent app to sparkweb

--- agent/src/main/java/com/github/nk/klusterfuck/agent/Application.java
+++ agent/src/main/java/com/github/nk/klusterfuck/agent/Application.java
@@ -1,28 +1,49 @@
 package com.github.nk.klusterfuck.agent;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import static spark.Spark.*;
 
-@SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+	public static void main(String[] args) throws Exception {
+		Config config = getconfig();
+		FunctionController ctrl = new FunctionController(config);
+		ctrl.init();
+
+		port(5000);
+
+		get("/", (req, res) -> ctrl.getFunctionConfig());
+
+		post("/", (req, res) -> {
+			String body = req.body();
+			return ctrl.run(body);
+		});
 	}
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurerAdapter() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry
-						.addMapping("*")
-						.allowedOrigins("*");
-			}
-		};
+	private static Config getconfig() {
+		Config config = new Config();
+		config.setWorkDir(getEnv("WORK_DIR"));
+		config.setGitUrl(getEnv("GIT_URL"));
+		config.setGogsUser(getEnv("GOGS_USER"));
+		config.setGogsPassword(getEnv("GOGS_PASSWORD"));
+		config.setCommitId(getEnv("GIT_COMMIT", ""));
+		config.setDisableCheckout(Boolean.valueOf(getEnv("DISABLE_CHECKOUT", "false")));
+		config.setDisableCache(Boolean.valueOf(getEnv("DISABLE_CACHE", "false")));
+		return config;
+	}
+
+	private static String getEnv(String name, String defaultVal) {
+		String env = System.getenv(name);
+		if (env == null || env.isEmpty()) {
+			return defaultVal;
+		}
+		return env;
+	}
+
+	private static String getEnv(String name) {
+		String env = System.getenv(name);
+		if (env == null) {
+			throw new RuntimeException("No value for required env var " + name);
+		}
+		return env;
 	}
 }
--- agent/src/main/java/com/github/nk/klusterfuck/agent/FunctionController.java
+++ agent/src/main/java/com/github/nk/klusterfuck/agent/FunctionController.java
@@ -1,40 +1,27 @@
 package com.github.nk.klusterfuck.agent;
 
 import com.github.nk.klusterfuck.common.FunctionConfig;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.*;
 
-import javax.annotation.PostConstruct;
-import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 import java.util.concurrent.*;
 
 /**
  * Created by nipunkumar on 02/06/17.
  */
-@RestController
-@RequestMapping({"", "/"})
 public class FunctionController {
 
-	@Value("${WORK_DIR}")
 	private String workDir;
-	@Value("${GIT_URL}")
 	private String gitUrl;
-	@Value("${GOGS_USER}")
 	private String gogsUser;
-	@Value("${GOGS_PASSWORD}")
 	private String gogsPassword;
 	/*
 	If not specified, commit id is set to "", and latest commit is used
 	 */
-	@Value("${GIT_COMMIT:}")
 	private String commitId;
-	@Value("${DISABLE_CHECKOUT:false}")
 	private boolean disableCheckout;
-	@Value("${DISABLE_CACHE:false}")
 	private boolean disableCache;
 
-	private FunctionConfig config;
+	private FunctionConfig functionConfig;
 	int corePoolSize = 20;
 	int maxPoolSize = 20;
 	int queueSize = 10;
@@ -42,12 +29,24 @@ public class FunctionController {
 
 	ExecutorService executor;
 
-	@PostConstruct
+	public FunctionController() {
+	}
+
+	public FunctionController(Config config) {
+		workDir = config.getWorkDir();
+		gitUrl = config.getGitUrl();
+		gogsUser = config.getGogsUser();
+		gogsPassword = config.getGogsPassword();
+		commitId = config.getCommitId();
+		disableCheckout = config.isDisableCheckout();
+		disableCache = config.isDisableCache();
+	}
+
 	public void init() throws Exception {
 		if (!disableCheckout) {
 			SetupUtils.setupClone(workDir, gitUrl, commitId, gogsUser, gogsPassword);
 		}
-		config = SetupUtils.readConfig(workDir);
+		functionConfig = SetupUtils.readConfig(workDir);
 		executor =
 				new ThreadPoolExecutor(
 						corePoolSize,
@@ -58,19 +57,15 @@ public class FunctionController {
 				);
 	}
 
-	@CrossOrigin()
-	@RequestMapping(method = RequestMethod.GET)
-	public FunctionConfig getConfig(HttpServletResponse response) {
-		return config;
+	public FunctionConfig getFunctionConfig() {
+		return functionConfig;
 	}
 
-	@CrossOrigin()
-	@RequestMapping(method = RequestMethod.POST, produces = "text/plain", consumes = "*/*")
-	public String run(@RequestBody(required = false) String body, HttpServletResponse response) throws Exception {
+	public String run(String body) throws Exception {
 		if (disableCache) {
-			config = SetupUtils.readConfig(workDir);
+			functionConfig = SetupUtils.readConfig(workDir);
 		}
-		String command = config.getCommand();
+		String command = functionConfig.getCommand();
 		if (command.startsWith("./")) {
 			// relative from work_dir
 			// first check if thee are arguments
@@ -78,7 +73,7 @@ public class FunctionController {
 			File file = new File(workDir, parts[0]);
 			command = file.getCanonicalFile().getAbsolutePath();
 			if (parts.length > 0) {
-				for (int  i = 1; i < parts.length; i++) {
+				for (int i = 1; i < parts.length; i++) {
 					command = command + " " + parts[i];
 				}
 			}
